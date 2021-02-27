ad8d423c33fef8e829611b92927cc28396cb8b93
RuanVanNiekerk <ruanvanniekerk3@gmail.com>
2021-02-27 16:51:59 +0200

    started replacing the existing Javascript with Vue.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,130 +1,97 @@
-let rand_num = [];
-//class for the product obj
-class product {
-    constructor(name = null, desc = null, price = null, image = null, category = null) {
-        this._name = name;
-        this._desc = desc;
-        this._price = price;
-        this._image = image;
-        this._category = category;
-    };
 
-    // method that fills the featured cards on the home page
-    static fill_featured() {
-
-        for (let i = 0; i < 9; i++) {
-            rand_num[i] = i;
+let vue_fashion = new Vue({
+    el: '#vue_container',
+    data: {
+        catalog: [
+            {
+                name: "Sandy Beige Melange",
+                desc: "Two-piece suit in sandy beige melange. Woven in wool, linen and polyester this weave creates a great feeling and structure.",
+                price: "399",
+                image: "TS-alsace-sand-51.jpg",
+                category: "suits"
+            },
+            {
+                name: "Navy blue blazer",
+                desc: "Navy blue blazer in 100% wool with discrete red and black lines woven into a nice square pattern.",
+                price: "215.2",
+                image: "TS-alsworth-navy-7.jpg",
+                category: "suits"
+            },
+            {
+                name: "Blue suit in houndstooth weave",
+                desc: "Two-piece suit in houndstooth weave. Woven in wool and linen for a comfortable and light feeling.",
+                price: "349",
+                image: "TS-ardtalla-blue-1.jpg",
+                category: "suits"
+            },
+            {
+                name: "Classic suit in black twill",
+                desc: "Three-piece classic suit in black twill with structure. A classic suit in 100% wool to wear for both business and formal events.",
+                price: "436",
+                image: "TS-ascot-black-7-1.jpg",
+                category: "suits"
+            },
+            {
+                name: "Gray glen check suit",
+                desc: "Two-piece gray glen check suit in 100% wool. This classic weave with black and purple lines creates a discrete and interesting check pattern.",
+                price: "299",
+                image: "TS-bridgetown-gray-10.jpg",
+                category: "suits"
+            },
+            {
+                name: "Classic check pattern",
+                desc: "Three-piece suit in gray with a classic check pattern in 100% wool. The suit jacket is designed with a classic lapel, single vent at back and front pockets with pocket lids.",
+                price: "436",
+                image: "TS-mayport-gray-31.jpg",
+                category: "suits"
+            },
+            {
+                name: "Classic tuxedo in black fine twill",
+                desc: "Two-piece classic tuxedo in black fine twill for the formal event. The jacket has a classic shawl lapel in satin and satin dressed buttons and pocket frames.",
+                price: "399",
+                image: "TS-Smoking-01.jpg",
+                category: "suits"
+            },
+            {
+                name: "Black fine twill with stretch",
+                desc: "Three-piece classic suit in black fine twill with stretch. The perfect suit for both formal and business events.",
+                price: "436",
+                image: "TS-traveller-black-11.jpg",
+                category: "suits"
+            },
+            {
+                name: "Beige checkered blazer",
+                desc: "Beige blazer in 100% wool with discrete glen check pattern in light blue. Designed with a peak lapel, single vent at back and front pockets. ",
+                price: "215.20",
+                image: "TS-albi-beige-peak-5.jpg",
+                category: "suits"
+            }
+        ],
+        cart_inv: [],
+        view_item: []
+    },
+    methods: {
+        //adds product to cart when you press the buy button
+        addProd: function (x) {
+            this.cart_inv.push(this.catalog[x]);
+            localStorage.setItem("cart", JSON.stringify(this.cart_inv));
+        },
+        viewProd: function (x) {
+            this.view_item.push(this.catalog[x]),
+            this.showProd();
+        },
+        showProd: function () {
+            console.log(this.view_item);
+            document.getElementById("view").style = "display:initial";
+        },
+        //closes the view div after cancel is clicked
+        viewClose: function () {
+            this.view_item.splice(0),
+            document.getElementById("view").style = "display: none;";
         }
-        rand_num.sort(this.random_sort);
-        for (let i = 0; i < 9; i++) {
-
-            let new_name = "card_" + i + "_name";
-            let new_desc = "card_" + i + "_desc";
-            let new_price = "card_" + i + "_price";
-            let new_img = "card_" + i + "_img";
-
-            document.getElementById(new_name).innerHTML = catalog[rand_num[i]].name;
-            document.getElementById(new_desc).innerHTML = catalog[rand_num[i]].desc;
-            document.getElementById(new_price).innerHTML = "$ " + catalog[rand_num[i]].price;
-            document.getElementById(new_img).src = "images/" + catalog[rand_num[i]].image;
-        }
     }
+});
 
-    static random_sort(a, b) {
-        return Math.random() - 0.5;
-    }
-
-    //updates the view div
-    static fill_view(e) {
-        let code = e.slice(5, 6);
-        let details = rand_num[code];
-
-        console.log(rand_num);
-
-        document.getElementById("view_name").innerHTML = catalog[details].name;
-        document.getElementById("view_desc").innerHTML = catalog[details].desc;
-        document.getElementById("view_price").innerHTML = "$ " + catalog[details].price;
-        document.getElementById("view_img").src = "images/" + catalog[details].image;
-    }
-
-    //add to shopping cart
-    static cart_add(e) {
-        console.log(e);
-        let code = e.slice(5, 6);
-        let cart_item = catalog[code];
-
-        let save_item = JSON.stringify(cart_item);
-        localStorage.setItem("Cart", save_item);
-    }
-}
-
-let product_obj = new product();
-// array of products
-let catalog = [
-    {
-        name: "Sandy Beige Melange",
-        desc: "Two-piece suit in sandy beige melange. Woven in wool, linen and polyester this weave creates a great feeling and structure.",
-        price: "399",
-        image: "TS-alsace-sand-51.jpg",
-        category: "suits"
-    },
-    {
-        name: "Navy blue blazer",
-        desc: "Navy blue blazer in 100% wool with discrete red and black lines woven into a nice square pattern.",
-        price: "215.2",
-        image: "TS-alsworth-navy-7.jpg",
-        category: "suits"
-    },
-    {
-        name: "Blue suit in houndstooth weave",
-        desc: "Two-piece suit in houndstooth weave. Woven in wool and linen for a comfortable and light feeling.",
-        price: "349",
-        image: "TS-ardtalla-blue-1.jpg",
-        category: "suits"
-    },
-    {
-        name: "Classic suit in black twill",
-        desc: "Three-piece classic suit in black twill with structure. A classic suit in 100% wool to wear for both business and formal events.",
-        price: "436",
-        image: "TS-ascot-black-7-1.jpg",
-        category: "suits"
-    },
-    {
-        name: "Gray glen check suit",
-        desc: "Two-piece gray glen check suit in 100% wool. This classic weave with black and purple lines creates a discrete and interesting check pattern.",
-        price: "299",
-        image: "TS-bridgetown-gray-10.jpg",
-        category: "suits"
-    },
-    {
-        name: "Classic check pattern",
-        desc: "Three-piece suit in gray with a classic check pattern in 100% wool. The suit jacket is designed with a classic lapel, single vent at back and front pockets with pocket lids.",
-        price: "436",
-        image: "TS-mayport-gray-31.jpg",
-        category: "suits"
-    },
-    {
-        name: "Classic tuxedo in black fine twill",
-        desc: "Two-piece classic tuxedo in black fine twill for the formal event. The jacket has a classic shawl lapel in satin and satin dressed buttons and pocket frames.",
-        price: "399",
-        image: "TS-Smoking-01.jpg",
-        category: "suits"
-    },
-    {
-        name: "Black fine twill with stretch",
-        desc: "Three-piece classic suit in black fine twill with stretch. The perfect suit for both formal and business events.",
-        price: "436",
-        image: "TS-traveller-black-11.jpg",
-        category: "suits"
-    },
-    {
-        name: "Beige checkered blazer",
-        desc: "Beige blazer in 100% wool with discrete glen check pattern in light blue. Designed with a peak lapel, single vent at back and front pockets. ",
-        price: "215.20",
-        image: "TS-albi-beige-peak-5.jpg",
-        category: "suits"
-    }
-];
 let cart = [];
 
 // for loops that create a array of each product category.
@@ -153,10 +120,6 @@ product.fill_featured();
 function view_item(e) {
     document.getElementById("view").style = "display: initial;";
     product.fill_view(e);
-}
-
-function view_close() {
-    document.getElementById("view").style = "display: none;";
 }
 
 function buy_item(e) {
